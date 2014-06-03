a046c7299537c40ae2b5eb5ce93c94a900436123
Sebastien Varrette <sebastien.varrette@uni.lu>
2014-06-03 16:13:28 +0200

    defaults loaders
    Signed-off-by: Sebastien Varrette <sebastien.varrette@uni.lu>

--- lib/falkorlib.rb
+++ lib/falkorlib.rb
@@ -1,10 +1,24 @@
+# -*- encoding: utf-8 -*-
+# Time-stamp: <Mar 2014-06-03 15:03 svarrette>
+################################################################################
+#                   _____     _ _              _     _ _
+#                   |  ___|_ _| | | _____  _ __| |   (_) |__
+#                   | |_ / _` | | |/ / _ \| '__| |   | | '_ \
+#                   |  _| (_| | |   < (_) | |  | |___| | |_) |
+#                   |_|  \__,_|_|_|\_\___/|_|  |_____|_|_.__/
 #
-# Sebastien Varrette aka Falkor's Common library to share Ruby code and `{rake,cap}` tasks
+################################################################################
+# Sebastien Varrette aka Falkor's Common library to share Ruby code and  
+# `{rake,cap}` tasks 
 #
+# * [Source code](https://github.com/Falkor/falkorlib)
+# * [Official Gem](https://rubygems.org/gems/falkorlib)
+################################################################################
+require "awesome_print"
+
 module FalkorLib
 
 end # module FalkorLib
 
 require "falkorlib/version"
 require "falkorlib/loader"
-require "falkorlib/common"
