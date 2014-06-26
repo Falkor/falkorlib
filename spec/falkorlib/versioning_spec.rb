738cc54d3bd68cd21a3c6c194c91249226415c4e
Sebastien Varrette <sebastien.varrette@uni.lu>
2014-06-26 23:24:14 +0200

    complete versioning
    Signed-off-by: Sebastien Varrette <sebastien.varrette@uni.lu>

--- spec/falkorlib/versioning_spec.rb
+++ spec/falkorlib/versioning_spec.rb
@@ -2,7 +2,7 @@
 #########################################
 # gitflow_spec.rb
 # @author Sebastien Varrette <Sebastien.Varrette@uni.lu>
-# Time-stamp: <Ven 2014-06-20 09:10 svarrette>
+# Time-stamp: <Jeu 2014-06-26 23:23 svarrette>
 #
 # @description Check the Git Flow operations -- see https://github.com/nvie/gitflow
 #
@@ -49,35 +49,52 @@ describe FalkorLib::Versioning do
             v.should == default_version
         end
 
-        # it "#get_version -- should get the '#{workingversion[:default]}' version set in the file #{versionfile}" do
-        #     execute "echo #{workingversion[:default]} > #{dir}/#{versionfile}"
-        #     v = FalkorLib::Versioning.get_version(dir)
-        #     v.should == workingversion[:default]
-        # end
+        it "#get_version -- should get the '#{workingversion[:default]}' version set in the file #{versionfile}" do
+            execute "echo #{workingversion[:default]} > #{dir}/#{versionfile}"
+            v = FalkorLib::Versioning.get_version(dir)
+            v.should == workingversion[:default]
+        end
 
-        # it "#set_version -- set version #{default_version} in version file #{versionfile}" do
-        #     STDIN.should_receive(:gets).and_return('no')
-        #     v = FalkorLib::Versioning.set_version(default_version, dir)
-        #     v.should == 0
-        #     v = FalkorLib::Versioning.get_version(dir)
-        #     v.should == default_version
-        # end
+		it "#major -- should collect the major version" do
+			v = FalkorLib::Versioning.get_version(dir)
+			m = FalkorLib::Versioning.major(v)
+			m.should == '1'
+		end
+		it "#minor -- should collect the minor version" do
+			v = FalkorLib::Versioning.get_version(dir)
+			m = FalkorLib::Versioning.minor(v)
+			m.should == '2'
+		end
+		it "#patch -- should collect the patch version" do
+			v = FalkorLib::Versioning.get_version(dir)
+			m = FalkorLib::Versioning.patch(v)
+			m.should == '3'
+		end
 
-        # FalkorLib.config[:versioning][:levels].reverse.each do |level|
-        #     it "#set_version #bump -- #{level} bump version number from #{workingversion[:default]} to #{workingversion[level.to_sym]}" do
-        #         # restore version file
-        #         execute "echo #{workingversion[:default]} > #{dir}/#{versionfile}"
-        #         v  = FalkorLib::Versioning.get_version(dir)
-        #         v.should == workingversion[:default]
-        #         v2 = FalkorLib::Versioning.bump(v, level.to_sym)
-        #         v2.should == workingversion[level.to_sym]
-        #         STDIN.should_receive(:gets).and_return('no')
-        #         d = FalkorLib::Versioning.set_version(v2, dir)
-        #         d.should == 0
-        #         v3 = FalkorLib::Versioning.get_version(dir)
-        #         v3.should == v2
-        #     end
-        # end
+
+        it "#set_version -- set version #{default_version} in version file #{versionfile}" do
+            STDIN.should_receive(:gets).and_return('no')
+            v = FalkorLib::Versioning.set_version(default_version, dir)
+            v.should == 0
+            v = FalkorLib::Versioning.get_version(dir)
+            v.should == default_version
+        end
+
+        FalkorLib.config[:versioning][:levels].reverse.each do |level|
+            it "#set_version #bump -- #{level} bump version number from #{workingversion[:default]} to #{workingversion[level.to_sym]}" do
+                # restore version file
+                execute "echo #{workingversion[:default]} > #{dir}/#{versionfile}"
+                v  = FalkorLib::Versioning.get_version(dir)
+                v.should == workingversion[:default]
+                v2 = FalkorLib::Versioning.bump(v, level.to_sym)
+                v2.should == workingversion[level.to_sym]
+                STDIN.should_receive(:gets).and_return('no')
+                d = FalkorLib::Versioning.set_version(v2, dir)
+                d.should == 0
+                v3 = FalkorLib::Versioning.get_version(dir)
+                v3.should == v2
+            end
+        end
 
         # it "should do something" do
 
