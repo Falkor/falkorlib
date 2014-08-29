41465efee952e240e4115b7625a699473e3dfc05
Sebastien Varrette <sebastien.varrette@uni.lu>
2014-08-29 14:32:27 +0200

    bump to version '0.3.2'
    Signed-off-by: Sebastien Varrette <sebastien.varrette@uni.lu>

--- lib/falkorlib/version.rb
+++ lib/falkorlib/version.rb
@@ -19,7 +19,7 @@ module FalkorLib #:nodoc:
         # MAJOR: Defines the major version
         # MINOR: Defines the minor version
         # PATCH: Defines the patch version
-	    MAJOR, MINOR, PATCH = 0, 3, 1  
+	    MAJOR, MINOR, PATCH = 0, 3, 2  
 	    
 	    module_function
 	    
