5e263bc59ca2e2e725bcfdd6ec31d3a050ca3ffc
Sebastien Varrette <sebastien.varrette@uni.lu>
2015-01-31 21:07:54 +0100

    Add Error classes
    Signed-off-by: Sebastien Varrette <sebastien.varrette@uni.lu>

---
+++ spec/falkorlib/error_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+require "falkorlib/error"
+
+
+describe FalkorLib::Error do
+
+    #############################################
+    context "Test Falkorlib errors" do
+
+        exceptions = [
+                      FalkorLib::Exit,
+                      FalkorLib::FalkorError,
+                      FalkorLib::ExecError,
+                      FalkorLib::InternalError,
+                      FalkorLib::ArgumentError,
+                      FalkorLib::AbortError,
+                      FalkorLib::TemplateNotFound
+                     ]
+        exceptions.each do |e|
+            it "##{e} -- exit code #{exceptions.index(e)}" do
+                expect { raise e }.to raise_error
+                begin
+                    raise e
+                rescue StandardError => s
+                    i = s.status_code
+                    i.should == exceptions.index(e)
+                end
+            end
+        end
+    end
+
+
+end
