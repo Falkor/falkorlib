2898553571b8b56581c1ae9e971f1632fd6c5d4c
Sebastien Varrette <sebastien.varrette@uni.lu>
2015-06-16 10:06:50 +0200

    add slides bootstrapping
    Signed-off-by: Sebastien Varrette <sebastien.varrette@uni.lu>

---
+++ templates/latex/beamer/.texinfo/main.tex.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook
+ "main.tex"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "_content"
+    "beamer"
+    "beamer10"
+    "_style")
+   (LaTeX-add-counters
+    "finalframe")))
+
