89027ec43db1e1523f23c60193cc4b5ec4b6b10b
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2016-11-10 01:59:35 +0100

    simplify rspec bootstrap_spec.rb
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- spec/falkorlib/bootstrap_spec.rb
+++ spec/falkorlib/bootstrap_spec.rb
@@ -2,7 +2,7 @@
 #########################################
 # bootstrap_spec.rb
 # @author Sebastien Varrette <Sebastien.Varrette@uni.lu>
-# Time-stamp: <Wed 2016-11-09 16:28 svarrette>
+# Time-stamp: <Wed 2016-11-09 20:17 svarrette>
 #
 # @description Check the basic Bootstrapping operations
 #
@@ -36,8 +36,8 @@ describe FalkorLib::Bootstrap do
     FalkorLib.config[:no_interaction] = false
   end
 
-  #[ :without_git, :with_git ].each do |ctx|
-  [ :with_git ].each do |ctx|
+  [ :without_git, :with_git ].each do |ctx|
+  #[ :with_git ].each do |ctx|
     dir = dirs[ctx]
     #############################################################
     context "bootstrap/base (#{ctx}) within temporary directory '#{dir}'" do
@@ -72,47 +72,6 @@ describe FalkorLib::Bootstrap do
         expect(c).to eq(0)
       end
 
-      # ######### RVM #########
-      # it "#rvm" do
-      #     gemset = File.basename(dir)
-      #     expect(STDIN).to receive(:gets).and_return('1') if ctx == :without_git
-      #     expect(STDIN).to receive(:gets).and_return('')  if ctx == :without_git
-      #     c = FalkorLib::Bootstrap.rvm(dir)
-      #     expect(c).to eq(0)
-      #     content = {}
-      #     [:versionfile, :gemsetfile].each do |type|
-      #         f = File.join(dir, FalkorLib.config[:rvm][type.to_sym])
-      #         t = File.exists?(f)
-      #         expect(t).to be true
-      #         content[type.to_sym] = `cat #{f}`.chomp
-      #     end
-      #     expect(content[:versionfile]).to eq(FalkorLib.config[:rvm][:rubies][0])
-      #     expect(content[:gemsetfile]).to  eq(gemset)
-      # end
-
-      # it "#rvm -- repeat" do
-      #     c = FalkorLib::Bootstrap.rvm(dir)
-      #     expect(c).to eq(1)
-      # end
-
-      # it "#rvm -- change targets (ctx = #{ctx}; dir = #{dir})" do
-      #     opts = {
-      #         :ruby        => '1.2.3',
-      #         :versionfile => '.myversion',
-      #         :gemset      => 'newgemset',
-      #         :gemsetfile  => '.mygemset'
-      #     }
-      #     c = FalkorLib::Bootstrap.rvm(dir, opts)
-      #     expect(c).to eq(0)
-      #     content = {}
-      #     [:versionfile, :gemsetfile].each do |type|
-      #         f = File.join("#{dir}", opts[type.to_sym])
-      #         t = File.exists?(f)
-      #         content[type.to_sym] = `cat #{f}`.chomp
-      #     end
-      #     expect(content[:versionfile]).to eq(opts[:ruby])
-      #     expect(content[:gemsetfile]).to  eq(opts[:gemset])
-      # end
 
       ### Bootstrap a VERSION file
       it "#versionfile" do
