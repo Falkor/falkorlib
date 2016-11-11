20f947e068e7034a57a75e67968b9fde56b75b67
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2016-11-11 14:30:08 +0100

    tab of 2
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- lib/falkorlib/git_tasks.rb
+++ lib/falkorlib/git_tasks.rb
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 ################################################################################
-# Time-stamp: <Jeu 2014-06-26 10:29 svarrette>
+# Time-stamp: <Fri 2016-11-11 14:29 svarrette>
 ################################################################################
 #
 # FalkorLib rake tasks to pilot Git [flow] operations
@@ -12,41 +12,41 @@ require 'falkorlib/tasks'
 
 module FalkorLib #:nodoc:
 
-    # Rake tasks to pilot Git operations
-    class GitTasks
-        include Rake::DSL if defined? Rake::DSL
+  # Rake tasks to pilot Git operations
+  class GitTasks
+    include Rake::DSL if defined? Rake::DSL
 
-        # Install the git[flow] tasks for Rake
-        def install_tasks
-            load 'falkorlib/tasks/git.rake'
-            load 'falkorlib/tasks/gitflow.rake'
-        end
-    end # class FalkorLib::GitTasks
+    # Install the git[flow] tasks for Rake
+    def install_tasks
+      load 'falkorlib/tasks/git.rake'
+      load 'falkorlib/tasks/gitflow.rake'
+    end
+  end # class FalkorLib::GitTasks
 end # module FalkorLib
 
 
 if FalkorLib::Git.init?
-    # Now install them ;)
-    FalkorLib::GitTasks.new.install_tasks
+  # Now install them ;)
+  FalkorLib::GitTasks.new.install_tasks
 else
-    warn "Git is not initialized for this directory."
-    warn "==> consider running 'rake git[:flow]:init' to be able to access the regular git Rake tasks"
+  warn "Git is not initialized for this directory."
+  warn "==> consider running 'rake git[:flow]:init' to be able to access the regular git Rake tasks"
+  #.....................
+  namespace :git do
+    ###########  git:init   ###########
+    desc "Initialize Git repository"
+    task :init do
+      FalkorLib::Git.init
+    end
+
     #.....................
-    namespace :git do
-        ###########  git:init   ###########
-        desc "Initialize Git repository"
-        task :init do
-            FalkorLib::Git.init
-        end
+    namespace :flow do
+      ###########  git:flow:init   ###########
+      desc "Initialize the Git-flow repository"
+      task :init do
+        FalkorLib::GitFlow.init
+      end
+    end # namespace git:flow
 
-        #.....................
-        namespace :flow do
-            ###########  git:flow:init   ###########
-            desc "Initialize the Git-flow repository"
-            task :init do
-                FalkorLib::GitFlow.init
-            end
-        end # namespace git:flow
-
-    end # namespace git
+  end # namespace git
 end
