3ec009c788cf4f6642ed1d80adab2eac1f535b43
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2016-11-11 14:22:04 +0100

    tab of 2
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- lib/falkorlib/error.rb
+++ lib/falkorlib/error.rb
@@ -1,28 +1,28 @@
 # -*- encoding: utf-8 -*-
 ################################################################################
-# Time-stamp: <Sam 2015-01-31 21:07 svarrette>
+# Time-stamp: <Fri 2016-11-11 14:21 svarrette>
 ################################################################################
 # Falkorlib errors
 
 module FalkorLib
 
-    # Errors
-    class Error < ::StandardError
+  # Errors
+  class Error < ::StandardError
 
-        class << self
-            attr_accessor :status_code
-        end
-
-        def status_code
-            self.class.status_code
-        end
+    class << self
+      attr_accessor :status_code
     end
 
-    class Exit             < Error; self.status_code = 0; end
-    class FalkorError      < Error; self.status_code = 1; end
-    class ExecError        < Error; self.status_code = 2; end
-    class InternalError    < Error; self.status_code = 3; end
-    class ArgumentError    < Error; self.status_code = 4; end
-    class AbortError       < Error; self.status_code = 5; end
-    class TemplateNotFound < Error; self.status_code = 6; end
+    def status_code
+      self.class.status_code
+    end
+  end
+
+  class Exit             < Error; self.status_code = 0; end
+  class FalkorError      < Error; self.status_code = 1; end
+  class ExecError        < Error; self.status_code = 2; end
+  class InternalError    < Error; self.status_code = 3; end
+  class ArgumentError    < Error; self.status_code = 4; end
+  class AbortError       < Error; self.status_code = 5; end
+  class TemplateNotFound < Error; self.status_code = 6; end
 end
